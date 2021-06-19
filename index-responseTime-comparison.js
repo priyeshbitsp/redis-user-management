20f1e60a363bb486cd0b0fc77dc7d750f023533a
Priyesh <jammula.priyesh@gmail.com>
2021-06-19 18:21:10 +0530

    Configuring redis port

--- index-responseTime-comparison.js
+++ index-responseTime-comparison.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const redis = require('redis');
 
 const PORT = process.env.PORT || 3000;
-const REDIS_PORT = process.env.PORT || 6379;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
 const client = redis.createClient(REDIS_PORT);
 const app = express();
