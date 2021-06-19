33efbe8e6894051098e13158b6133054d1c2a2e7
Priyesh <jammula.priyesh@gmail.com>
2021-06-19 16:47:31 +0530

    CRUD operations

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Redis User Management
+## Usage
+Simple user management app using Node.js and Redis
+## Things to keep in mind before running
+Make sure you have Redis installed and running
+## Install Dependencies
+$ npm install
+## Run Server
+$ npm start
+
+## Visit http://localhost:3000
--- app.js
+++ app.js
@@ -34,7 +34,7 @@ it must be used prior to the csurf module).
 -> methodOverride(getter, options)
 Create a new middleware function to override the req.method property with a new value. This value will be pulled from the provided getter
 */
-app.use(methodOverride('_method')); // "_method" is the parameter to be used while we make a delte request from a form
+app.use(methodOverride('_method')); // "_method" is the parameter to be used while we make a delete request from a form using POST request
 
 // Search Page
 app.get('/', function(req, res, next){
@@ -47,8 +47,9 @@ app.post('/user/search', function(req, res, next){
 
     client.hgetall(id, function(err, obj){
         if(!obj){
-        res.render('searchusers', {
-            error: 'User does not exist'
+            // If no user, show searchusers with an error
+            res.render('searchusers', {
+                error: 'User does not exist'
         });
         } else {
         obj.id = id;
@@ -57,6 +58,40 @@ app.post('/user/search', function(req, res, next){
         });
         }
     });
+});
+
+// Add User Page
+app.get('/user/add', function(req, res, next){
+    res.render('adduser');
+});
+
+// Process Add User Page
+app.post('/user/add', function(req, res, next){
+    let id = req.body.id;
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+    let email = req.body.email;
+    let phone = req.body.phone;
+  
+    client.hmset(id, [
+      'first_name', first_name,
+      'last_name', last_name,
+      'email', email,
+      'phone', phone
+    ], function(err, reply){
+      if(err){
+        console.log("Error from redis service: ", err);
+      }
+      console.log("Reply from redis service: ", reply);
+      res.redirect('/');
+    });
+});
+  
+// Delete User
+// We are actually sending POST request from form to delete the user, but we override using method-override and param as _method
+app.delete('/user/delete/:id', function(req, res, next){
+    client.del(req.params.id);
+    res.redirect('/');
 });
 
 app.listen(port, function(){
